8fc0d8bc6f984085258c7e6c6e0faaf089574c59
tomar2022 <tomarjrobinson@gmail.com>
2022-09-26 15:04:09 -0400

    first commit

---
+++ Howold.java
@@ -0,0 +1,62 @@
+package classProjectWhatsyourName;
+import java.util.*;
+
+
+public class Howold {
+
+	public static void main(String[] args) {
+		
+	Scanner scan = new Scanner(System.in);
+	System.out.println ("What's Your Name?");
+	String name = " What's Your Name ";
+	String person = "Tomar";
+	System.out.println("What's your name"+ name.toUpperCase());
+	System.out.println("What's your name" + name.toLowerCase());
+	System.out.println("What's your name" + name.charAt(8));
+	System.out.println("What's your name" + name.length());
+	System.out.println("What's your name" + name.charAt(6));
+	
+	
+	
+	
+	
+	
+	
+	String id = scan.next();
+	
+	
+	
+	
+	
+	System.out.println("What's Your Age");
+	int age = scan.nextInt();
+	
+	
+	System.out.println ("How long do you want to live?");
+	
+	int live = scan.nextInt();
+	int a = (50);
+	 int b = (100);
+	 int c = (200);
+	 int d = (300);
+	 double e = (55);
+	 
+	 System.out.println(a+b);
+	 System.out.println(c-d);
+	 System.out.println(d*e);
+	 System.out.println(e/a);
+	 System.out.println(d%a);
+	 
+	 
+	 
+	System.out.println("May The Force Be With YOU!" + person);
+	
+	 
+	
+	
+	
+			
+
+	}
+
+}
